223d9d044f4c4b62829c0383597964c68ef0047b
Sal Estrada <sal.estrada21@gmail.com>
2025-09-29 16:11:41 -0400

    Add Validation

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-mongoose.connect('mongodb://localhost:27017/mfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/mfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.static('documentation'));
 
